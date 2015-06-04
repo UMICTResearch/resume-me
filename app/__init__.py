45bc90ba2eaeb8bec85f932a1d023c45b0cf27d0
nishanbose <nishan@umich.edu>
2015-06-04 16:50:46 -0400

    Updating refactored code - views and init

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,16 @@
+#
+# Copyright SIG - June 2015
+#
+
 import os
 import sys
 
-from flask import Flask, render_template
+from flask import Flask, url_for, request, session, redirect, render_template, make_response, current_app, jsonify, \
+    send_from_directory
+from logger import logger
+from dbclient import db, client
+from urllib2 import urlopen
+from functools import update_wrapper, wraps
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -30,8 +39,54 @@ def install_secret_key(app, filename='secret_key'):
         print('head -c 24 /dev/urandom > {filename}'.format(filename=filename))
         sys.exit(1)
 
+
 if not app.config['DEBUG']:
     install_secret_key(app)
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
+
+
+def support_jsonp(f):
+    """Wraps JSONified output for JSONP"""
+
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        callback = request.args.get('callback', False)
+        if callback:
+            content = str(callback) + '(' + str(f().data) + ')'
+            return app.response_class(content, mimetype='application/json')
+        else:
+            return f(*args, **kwargs)
+
+    return decorated_function
+
+
+def get_header_ip():
+    if request.headers.get("REMOTE_ADDR"):
+        return request.headers.get('REMOTE_ADDR')
+    else:
+        return request.remote_addr
+
+
+def record_actions(session):
+    # logr.plog("Data: "+ json.dumps(session, default=json_util.default), class_name='action')
+    # find task_type and operation
+
+    # logr.plog("Request is by %s."%(get_header_ip()), class_name='record_actions')
+    session['__name__'] = __name__
+    session['worker_ip'] = get_header_ip()
+    if client:
+        try:
+            res = db.operations.insert(session)
+            logr.plog("Successfully pushed a response to MongoDB. Document _id is %s." % res,
+                      class_name='record_actions')
+        except Exception as e:
+            logr.plog("Couldnt push the response to DB. Exceptions: %s" % e, class_name='record_actions')
+    else:
+        logr.plog("Couldn't connect to MongoDB. Please check the connection.", class_name='record_actions')
 
 
 @app.errorhandler(404)
--- run.py
+++ run.py
@@ -3,4 +3,12 @@
 #
 
 from app import app
-app.run(debug=True)
+from logger import logger
+
+if __name__ == "__main__":
+    logr = logger('./Logs', 'resume-feedback-dev', insertDate=False)
+    app.run(host='0.0.0.0', port=4999, debug=True)  #
+    # else:
+    #     # init logger
+    #     app.wsgi_app = ProxyFix(app.wsgi_app)
+    #     logr = logger('./Logs', 'resume-feedback-server', insertDate=False)
