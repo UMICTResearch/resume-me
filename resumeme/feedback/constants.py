57ad8fe8a7b2c26f06492bdcb5f6d83b37b2cdbc
Suleman Diwan <suledwan@gmail.com>
2015-07-01 00:35:37 -0400

    Add Feedback Module

---
+++ resumeme/feedback/constants.py
@@ -0,0 +1,10 @@
+# Rating Field
+MAX_RATING_LENGTH = 1
+
+# Section Field
+FIRST_SECTION = "Overall Feedback"
+SECOND_SECTION = "Formatting Issues"
+THIRD_SECTION = "Education"
+FOURTH_SECTION = "Experience"
+FIFTH_SECTION = "Skills/Misc"
+MAX_SECTION_NAME_LENGTH = 64
