a4d9d6282b9dd6a822fa694f50e31855ee93c832
Suleman Diwan <suledwan@gmail.com>
2015-08-30 02:46:27 -0400

    Seeker Review Feature

--- resumeme/feedback/constants.py
+++ resumeme/feedback/constants.py
@@ -5,9 +5,20 @@ MAX_RATING_LENGTH = 1
 MIN_CONTENT_LENGTH = 2
 
 # Section Field
-FIRST_SECTION = "Overall Feedback"
+LEGACY_SECTION = "Overall Feedback"
+FIRST_SECTION = "Overall, the skills and experience listed on this resume match the job description specified."
 SECOND_SECTION = "Formatting Issues"
 THIRD_SECTION = "Education"
 FOURTH_SECTION = "Experience"
 FIFTH_SECTION = "Skills/Misc"
-MAX_SECTION_NAME_LENGTH = 64
+MAX_SECTION_NAME_LENGTH = 128
+
+# Simple numbers to represent the values
+#
+CHOICE_ONE = "1"
+CHOICE_TWO = "2"
+CHOICE_THREE = "3"
+
+SECTION_CHOICE_ONE = "1"
+SECTION_CHOICE_TWO = "2"
+SECTION_CHOICE_THREE = "3"
