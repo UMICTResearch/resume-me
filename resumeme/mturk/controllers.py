b4d71a22c9eddd2d5b69f2f16384cf9faaea4592
VictorYYW <slark@umich.edu>
2019-10-13 22:36:12 -0400

    make automation process reliable by keeping track of each resume

--- resumeme/mturk/controllers.py
+++ resumeme/mturk/controllers.py
@@ -6,6 +6,7 @@ from datetime import datetime
 from threading import Timer
 from resumeme.libs.User import User
 from resumeme.utils.controllers import send_mail
+from bson.objectid import ObjectId
 import urllib
 
 import models
@@ -55,120 +56,82 @@ def mturk_feedback_main():
         user = register_mturk()
     login_user(user, remember="no")
 
-    resume_id = ''
-
-    user_resume_list = models.Resume.objects(
-        db_query(lock=False)
-        )
-
-    if len(user_resume_list) != 0:
-
-        for resume in user_resume_list:
-
-            # Check if timedelta between the current time and the last-reviewed time is already over one hour
-            if (datetime.now()-resume.last_reviewed).seconds > 3600:
-                resume_requested = resume
-                resume_id = resume.id
-                break
-            else:
-                continue
-
-        if resume_id == '':
-            # No resume is avialable
-            return render_template('no_resume.html')
-
-        current_resume = models.Resume.objects().with_id(resume_id)
-
-        if request.method == "POST":
-
-            current_id = request.form.get('resume_id')
-            current_resume = models.Resume.objects().with_id(current_id)
-
-            if current_resume.lock is False:
-                try:
-                    # Tells the feedback display page that the feedback was freshly created and saved.
-                    state = "saved"
-                    created = datetime.now()
-
-                    feedback = models.Feedback()
-                    feedback.last_updated = created
-
-                    feedback.first_section = models._Section()
-                    feedback.second_section = models._Section()
-                    feedback.third_section = models._Section()
-                    feedback.fourth_section = models._Section()
-                    feedback.fifth_section = models._Section()
-
-                    feedback.resume = current_resume
-
-                    feedback.first_section.name = CONSTANTS.FIRST_SECTION
-                    feedback.first_section.rating = request.form.get('rating_1')
-                    feedback.first_section.content = request.form.get('content_1')
-                    feedback.second_section.name = CONSTANTS.SECOND_SECTION
-                    feedback.second_section.rating = request.form.get('rating_2')
-                    feedback.second_section.content = request.form.get('content_2')
-                    feedback.third_section.name = CONSTANTS.THIRD_SECTION
-                    feedback.third_section.rating = request.form.get('rating_3')
-                    feedback.third_section.content = request.form.get('content_3')
-                    feedback.fourth_section.name = CONSTANTS.FOURTH_SECTION
-                    feedback.fourth_section.rating = request.form.get('rating_4')
-                    feedback.fourth_section.content = request.form.get('content_4')
-                    feedback.fifth_section.name = CONSTANTS.FIFTH_SECTION
-                    feedback.fifth_section.rating = request.form.get('rating_5')
-                    feedback.fifth_section.content = request.form.get('content_5')
-
-                    feedback.validate()
-
-                    feedback.resume.update(lock=True)
-
-                    # associate feedback to resume owner
-                    feedback.user = feedback.resume.user
-
-                    feedback.save()
-
-
-                    # push feedback onto resume feedback_list reference list
-                    models.Resume.objects(id=resume_id).update_one(push__feedback_list=feedback)
-                    current_resume.save()
-
-                    url_para = 'workerId=' + worker_id + '&assignmentId=' + assignment_id + '&hitId=' + 'hit_id' + '&turkSubmitTo=' + submit_hit_url
-
-                    return redirect('/mturk/%s/%s/%s?%s' % (feedback.resume.id, feedback.id, state, url_para))
-
-
-
-                except ValidationError as e:
-                    print "Error:", e
-                    flash('Fill out all fields')
-                    template_data = {
-                        'title': 'Give Feedback',
-                        'content': None,
-                        'resume': models.Resume.objects().with_id(resume_id)
-                    }
-                    return render_template('mturk/edit.html', **template_data)
-
-            elif current_resume.lock is True:
-                # Resume has been reviewed
-                return render_template('404.html')
-
-        else:
+    if request.method == "GET":
+        resume_id = models.boto3_client.get_hit(
+            HITId=hit_id
+        )['HIT']['RequesterAnnotation']
+        resume_id = ObjectId(resume_id)
+        resume = models.Resume.objects().with_id(resume_id)
+        if resume != None:
             # Default
             template_data = {
                 'title': 'Give Feedback',
                 'content': None,
-                'resume': models.Resume.objects().with_id(resume_id)
+                'resume': resume,
             }
-
-            # Update the last_reviewed timestamp
-            # No update in preview mode
-            if assignment_id != "ASSIGNMENT_ID_NOT_AVAILABLE":
-                current_resume.update(last_reviewed = datetime.now())
-
             return render_template('mturk/edit.html', **template_data)
-
+        else:
+            return render_template('no_resume.html')
     else:
-        # No resume is avialable
-        return render_template('no_resume.html')
+        resume_id = request.form.get('resume_id')
+        resume = models.Resume.objects().with_id(resume_id)
+
+        try:
+            # Tells the feedback display page that the feedback was freshly created and saved.
+            state = "saved"
+            created = datetime.now()
+
+            feedback = models.Feedback()
+            feedback.last_updated = created
+
+            feedback.first_section = models._Section()
+            feedback.second_section = models._Section()
+            feedback.third_section = models._Section()
+            feedback.fourth_section = models._Section()
+            feedback.fifth_section = models._Section()
+
+            feedback.resume = resume
+
+            feedback.first_section.name = CONSTANTS.FIRST_SECTION
+            feedback.first_section.rating = request.form.get('rating_1')
+            feedback.first_section.content = request.form.get('content_1')
+            feedback.second_section.name = CONSTANTS.SECOND_SECTION
+            feedback.second_section.rating = request.form.get('rating_2')
+            feedback.second_section.content = request.form.get('content_2')
+            feedback.third_section.name = CONSTANTS.THIRD_SECTION
+            feedback.third_section.rating = request.form.get('rating_3')
+            feedback.third_section.content = request.form.get('content_3')
+            feedback.fourth_section.name = CONSTANTS.FOURTH_SECTION
+            feedback.fourth_section.rating = request.form.get('rating_4')
+            feedback.fourth_section.content = request.form.get('content_4')
+            feedback.fifth_section.name = CONSTANTS.FIFTH_SECTION
+            feedback.fifth_section.rating = request.form.get('rating_5')
+            feedback.fifth_section.content = request.form.get('content_5')
+
+            feedback.validate()
+
+            # associate feedback to resume owner
+            feedback.user = feedback.resume.user
+            feedback.save()
+
+            # push feedback onto resume feedback_list reference list
+            resume.update(push__feedback_list=feedback)
+            resume.update(last_reviewed=datetime.now())
+            resume.update(posted=False)
+
+            url_para = 'workerId=' + worker_id + '&assignmentId=' + assignment_id + '&hitId=' + 'hit_id' + '&turkSubmitTo=' + submit_hit_url
+
+            return redirect('/mturk/%s/%s/%s?%s' % (feedback.resume.id, feedback.id, state, url_para))
+
+        except ValidationError as e:
+            print "Error:", e
+            flash('Fill out all fields')
+            template_data = {
+                'title': 'Give Feedback',
+                'content': None,
+                'resume': resume
+            }
+            return render_template('mturk/edit.html', **template_data)
 
 
 @mturk.route('/mturk/<resume_id>/<feedback_id>/<state>')
@@ -178,12 +141,10 @@ def mturk_entry_page(resume_id, feedback_id, state="view"):
     resume = models.Resume.objects().with_id(resume_id)
     feedback = models.Feedback.objects().with_id(feedback_id)
 
-
     if resume and feedback:
         # Display this only when the feedback is freshly saved and not when it is just being viewed.
         if state == "saved":
             flash('Feedback has been saved')
-
         else:
             feedback.viewed = True
             feedback.save()
@@ -200,16 +161,11 @@ def mturk_entry_page(resume_id, feedback_id, state="view"):
         return render_template('404.html')
 
 
-def get_resume_length():
-    resume_list = models.Resume.objects()
-    print(len(resume_list))
-
-
 def mturk_post_HIT():
     resume_list = models.Resume.objects(
         db_query(posted=False)
     )
-
+    count = 0
     for resume in resume_list:
         if (datetime.now()-resume.last_reviewed).seconds > 3600:
             resume.update(posted=True)
@@ -223,8 +179,45 @@ def mturk_post_HIT():
                 Keywords='resume, review, rate',
                 Description='Please review resume, rate it and give feedback',
                 Question=CONSTANTS.EXTERNAL_QUESTION,
+                RequesterAnnotation=str(resume.id),
                 QualificationRequirements=CONSTANTS.WORKER_REQUIREMENTS,
             )
+            count += 1
+    print('{} HITs posted'.format(count))
+
+
+def mturk_check_status():
+    response = models.boto3_client.list_hits()
+    num_hits = response['NumResults']
+    for HIT in response['HITs']:
+        print(HIT['HITStatus'])
+        assignments = models.boto3_client.list_assignments_for_hit(
+            HITId=HIT['HITId'],
+        )['Assignments']
+        for assignment in assignments:
+            print(assignment['AssignmentStatus'])
+
+
+def mturk_clean_HIT():
+    """
+    For each reviewable HIT,
+    approve all submitted assignments, and delete HIT.
+    Note: some HITs expire and don't have any assignment.
+    """
+    response = models.boto3_client.list_reviewable_hits()
+    HITs = response['HITs']
+    for HIT in HITs:
+        assignments = models.boto3_client.list_assignments_for_hit(
+            HITId=HIT['HITId'],
+            AssignmentStatuses=['Submitted'],
+        )['Assignments']
+        for assignment in assignments:
+            models.boto3_client.approve_assignment(
+                AssignmentId=assignment['AssignmentId'],
+            )
+        models.boto3_client.delete_hit(
+            HITId=HIT['HITId']
+        )
 
     
 def mturk_report():
@@ -234,6 +227,4 @@ def mturk_report():
             
 @mturk.record
 def add_mturk_job(state):
-    #state.app.scheduler.add_job(func=get_resume_length, trigger="interval", seconds=5)
-    #state.app.scheduler.add_job(func=mturk_post_HIT, trigger="interval", seconds=10)
-    pass
+    state.app.scheduler.add_job(func=mturk_post_HIT, trigger="interval", seconds=3600)
